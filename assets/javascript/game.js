481a9b3ae8f26131e6c7317c5cfd77f037e33c1a
Abusch419 <abusch419@gmail.com>
2019-04-21 17:50:45 -0400

    updates index.html and style.css with partial game functionality

---
+++ assets/javascript/game.js
@@ -0,0 +1,17 @@
+// define global variables so that the game is stateful and can track wins losses and points 
+
+// generate a random number for the target score between 1 and 100 and display it in the random -number div
+
+// generate a random number for each of the four crystals
+
+// create a click event listener for each crystal 
+
+// add points to the total score each time a crystal is cliecked and update the score div to keep track
+
+// if user score is less than target score allow another click 
+// if user score is more than target score say you lost! 
+
+
+
+
+
