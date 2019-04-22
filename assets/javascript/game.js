ba6f3b16869f411686bb0adcc6197900e39fc715
Abusch419 <abusch419@gmail.com>
2019-04-22 08:37:44 -0400

    updates index.html style.css and game.js with full game functionality and removed js from html files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,91 @@
-// define global variables so that the game is stateful and can track wins losses and points 
-
-// generate a random number for the target score between 1 and 100 and display it in the random -number div
-
-// generate a random number for each of the four crystals
-
-// create a click event listener for each crystal 
-
-// add points to the total score each time a crystal is cliecked and update the score div to keep track
-
-// if user score is less than target score allow another click 
-// if user score is more than target score say you lost! 
 
 
+var counter = 0;
+let wins = 0;
+let losses = 0;
+let targetNumber = 0;
+
+$(document).ready(function () {
+
+    // generate random number and write to the random number div 
+
+
+    function resetTargetNumber() {
+        targetNumber = Math.floor(Math.random() * (100 - 1 + 1) + 1);
+        $("#random-number").text(targetNumber);
+    }
+
+    resetTargetNumber();
+
+    function resetCrystalValues() {
+        //   set random value for crystal one 
+        let crystalOne = Math.floor(Math.random() * (20 - 1 + 1) + 1);
+        $("#crystalOne").attr("data-number", crystalOne);
+        //   set random value for crystal two
+        let crystalTwo = Math.floor(Math.random() * (20 - 1 + 1) + 1);
+        $("#crystalTwo").attr("data-number", crystalTwo);
+        //   set random value for crystal three
+        let crystalThree = Math.floor(Math.random() * (20 - 1 + 1) + 1);
+        $("#crystalThree").attr("data-number", crystalThree);
+        //   set random value for crystal four
+        let crystalFour = Math.floor(Math.random() * (20 - 1 + 1) + 1);
+        $("#crystalFour").attr("data-number", crystalFour);
+    }
+
+    resetCrystalValues();
+
+    // write a set counter function 
+
+    function setCounter(newCounter) {
+        counter = newCounter;
+        $("#score-counter").text(counter);
+    }
+
+    // write a game reset function 
+
+    function resetGame() {
+        resetCrystalValues();
+        resetTargetNumber();
+        setCounter(0);
+    }
+
+    function incrementWins() {
+        wins++;
+        $("#win-counter").text(wins);
+    }
+
+    function incrementLosses() {
+        losses++;
+        $("#loss-counter").text(losses);
+    }
+
+    // create function to add points to counter with each crystal click and determine a win vs a loss
+
+    $(".crystal-image").on("click", function () {
+
+        var crystalPoints = ($(this).attr("data-number"));
+        crystalPoints = parseInt(crystalPoints);
+        setCounter(counter + crystalPoints);
+        console.log("Click!");
+
+
+
+        if (counter > targetNumber) {
+            incrementLosses();
+            $("#outcome").text("You Lost!");
+            console.log("You lost!")
+            resetGame();
+
+
+        } else if (counter === targetNumber) {
+            incrementWins();
+            $("#outcome").text("You Won!");
+            resetGame();
+
+        }
+
+    });
+})
 
 
 
