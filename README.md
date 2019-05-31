865fc8863ecd81720eefffac94f7c801f05c7542
Abusch419 <abusch419@gmail.com>
2019-05-30 21:16:39 -0400

    updates style and readme

--- README.md
+++ README.md
@@ -1,4 +1,30 @@
-# unit-4-game
+# Crytal Counter
 
-Deployed Link
-https://abusch419.github.io/unit-4-game/
+A basic game of memory. There are four crystals below. By clicking on a crystal you will add a scpecific amount of points to your total score. You win the game by matching your total score to random number, you lose the game if your total score goes above the random number. The value of each crystal is hidden from you until you click on it. Each time when the game starts, the game will change the value of each crystal.
+ 
+
+## Deployment
+
+This app was deployed using GitHub. 
+githubmasterlink!!!!!!!
+
+## Built With
+
+* [Bootstrap](https://getbootstrap.com/docs/4.3/getting-started/introduction/)
+* [jQuery](https://api.jquery.com/)
+
+## Authors
+
+* **Alex Eversbusch** 
+
+## Version
+
+V1.0
+
+## Screenshots
+## Start Page
+![](./assets/images/homepage.png)
+## Game Page
+![](./assets/images/gamepage.png)
+
+
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,99 @@
-
-
-var counter = 0;
-let wins = 0;
-let losses = 0;
-let targetNumber = 0;
-
 $(document).ready(function () {
 
+    $(".game-div").hide()
+    var counter = 0;
+    let wins = 0;
+    let losses = 0;
+    let targetNumber = 0;
+
+
+
+
+
+    $(".start-game").on("click", function (e) {
+        e.preventDefault()
+        $(".game-div").show()
+        hideInstructions()
+        renderGame()
+        resetTargetNumber()
+        resetCrystalValues()
+        PlayGame()
+    })
+
+    function PlayGame() {
+        $(".crystal-image").on("click", function () {
+
+            var crystalPoints = ($(this).attr("data-number"));
+            crystalPoints = parseInt(crystalPoints);
+            setCounter(counter + crystalPoints);
+            console.log("Click!");
+
+
+            $("#outcome").empty()
+            if (counter > targetNumber) {
+                incrementLosses();
+                $("#outcome").text("You Lost!");
+                console.log("You lost!")
+                resetGame();
+
+
+            } else if (counter === targetNumber) {
+                incrementWins();
+                $("#outcome").text("You Won!");
+                resetGame();
+
+            }
+
+        })
+    }
+
+
+
+
     // generate random number and write to the random number div 
+
+    function hideInstructions() {
+        $(".instructions").hide()
+    }
+
+    function renderGame() {
+        $(".game-div").empty()
+        console.log("renderGame")
+        let game =
+        `
+            <div class="game row">
+            <h1 class="col-12" id="target-number">Target Number</h1>
+            <h3 class="col-12" id="random-number">
+            </h3>
+            <div class="col-12" id="crystal-images">
+                <img class="crystal-image col-xs-6 col-sm-6 col-md-3" id="crystalOne" src="assets/images/crystal1.png" alt="crystal 1">
+                <img class="crystal-image col-xs-6 col-sm-6 col-md-3" id="crystalTwo" src="assets/images/crystal2.png" alt="crystal 2">
+                <img class="crystal-image col-xs-6 col-sm-6 col-md-3" id="crystalThree" src="assets/images/crystal3.png" alt="crystal 3">
+                <img class="crystal-image col-xs-6 col-sm-6 col-md-3" id="crystalFour" src="assets/images/crystal4.png" alt="crystal 4">
+            </div>
+            <div class="col-6 card">
+                    <div class="card-header">
+                        Points
+                    </div>
+                    <div class="card-body" id="score-counter">
+    
+                    </div>
+            </div>
+            <div class="col-6 card">
+                    <div class="card-header">
+                        Score
+                    </div>
+                    <div class="card-body" id="wins-losses">
+                        <p id="outcome"></p>
+                        <p id="win-text">Wins:<span id="win-counter">0</span> </p>
+                        <p id="loss-text">Losses:<span id="loss-counter">0</span> </p>
+                    </div>
+            </div>
+        </div>
+    
+        `
+        $(".game-div").append(game)
+    }
 
 
     function resetTargetNumber() {
@@ -15,7 +101,7 @@ $(document).ready(function () {
         $("#random-number").text(targetNumber);
     }
 
-    resetTargetNumber();
+
 
     function resetCrystalValues() {
         //   set random value for crystal one 
@@ -32,7 +118,7 @@ $(document).ready(function () {
         $("#crystalFour").attr("data-number", crystalFour);
     }
 
-    resetCrystalValues();
+
 
     // write a set counter function 
 
@@ -61,30 +147,7 @@ $(document).ready(function () {
 
     // create function to add points to counter with each crystal click and determine a win vs a loss
 
-    $(".crystal-image").on("click", function () {
 
-        var crystalPoints = ($(this).attr("data-number"));
-        crystalPoints = parseInt(crystalPoints);
-        setCounter(counter + crystalPoints);
-        console.log("Click!");
-
-
-
-        if (counter > targetNumber) {
-            incrementLosses();
-            $("#outcome").text("You Lost!");
-            console.log("You lost!")
-            resetGame();
-
-
-        } else if (counter === targetNumber) {
-            incrementWins();
-            $("#outcome").text("You Won!");
-            resetGame();
-
-        }
-
-    });
 })
 
 
